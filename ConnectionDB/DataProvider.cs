f7c0d008c6792213ee36713043783f1a3f6a76b4
benhat907 <benhat907@gmail.com>
2023-05-07 17:14:48 +0700

    fm

--- ConnectionDB/DataProvider.cs
+++ ConnectionDB/DataProvider.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using Microsoft.Data.SqlClient;
 
-namespace Faultygoodslibrary
+namespace Lab1
 {
     public static class DataProvider
     {
@@ -11,24 +11,49 @@ namespace Faultygoodslibrary
         {
             try
             {
-                string connectionString = @"Data Source = .\sqlexpress; Initial Catalog = QLFGL, User = sa, Password = 1";
+                string connectionString = @"Data Source = .\sqlexpress; Initial Catalog = QLSV; User= sa; Password=1";
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
-                if(connection.State != System.Data.ConnectionState.Open)
+                if (connection.State != System.Data.ConnectionState.Open)
                     return new DataTable();
                 SqlCommand cmd = new SqlCommand(query, connection);
                 foreach (var pram in parameters)
                 {
-                    cmd.Parameters.AddWithValue(pram.Key, pram.Value);
+                    cmd.Parameters.AddWithValue(pram.Key, pram.Value);               
                 }
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 DataTable dataTable = new DataTable();
                 da.Fill(dataTable);
                 return dataTable;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return new DataTable();
+            }
+
+        }
+        public static int ExecuteNonQuery(String query, Dictionary<string, object> parameters = null)
+        {
+            try
+            {
+                string connectionString = @"Data Source = .\sqlexpress; Initial Catalog = QLSV; User= sa; Password=1";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                if (connection.State != System.Data.ConnectionState.Open)
+                    return -1;
+                SqlCommand cmd = new SqlCommand(query, connection);
+                if (parameters != null)
+                {
+                    foreach (var pram in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(pram.Key, pram.Value);
+                    }
+                }
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                return -1;
             }
         }
     }
